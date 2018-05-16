44f908c3d80c05895ca9be0ffde1ff1fa3fe81d8
Chap-z <cha.lacroix@hotmail.Fr>
2018-05-16 09:12:32 +0200

    add view

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
