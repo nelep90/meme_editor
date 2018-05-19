264c689a73c9c3fe9f8b70a0e99fdcd3c5b2b2cd
yannickACS <yannick.k@codeur.online>
2018-05-19 18:17:28 +0200

    normalisation utf8 sur bdd, désolé teammates vous allez devoir retoucher vos logins

--- models/Bddmanager.php
+++ models/Bddmanager.php
@@ -3,13 +3,14 @@
 class Bddmanager
 {
     protected $bdd;
-    private $host = "127.0.0.1";
+    private $host = "localhost";
     private $login = "root";
-    private $password = "root";
+    private $password = "";
  
     public function __construct()
     {
         $bdd = new PDO('mysql:host=' . $this->host . ';dbname=meme_editor;charset=utf8', $this->login, $this->password);
+        $bdd->exec("SELECT CHARACTER SET utf8");
         $this->bdd = $bdd;
  
     }
