7d4a35ff958811aaf1de4235fbdc8fa62a7c3026
yannickACS <yannick.k@codeur.online>
2018-05-20 00:29:59 +0200

    ajout commentaires sur router

--- index.php
+++ index.php
@@ -21,17 +21,17 @@ if (isset($_GET['action'])){
         require_once('controllers/Controller_home.php');
         $action = new Controller_home();
         $action->action_layout();
-
         break;
         // Page Générateur de Meme
         case 'generator':
         require_once('controllers/Controller_generator.php');
+                    // route vers upload Image
         if ((isset($_GET['upload'])) && ($_GET['upload'] == "true")){
             if ((isset($_FILES['uploadImage'])) && (!empty($_FILES['uploadImage']))){
                 $action = new Controller_generator();
                 $action->action_upload();                
             }
-        } else {
+        } else {    // defini l'image mis en avant
             if ((isset($_GET['id'])) && (!empty($_GET['id']))){
                 $id = (int)$_GET['id'];
                 $action = new Controller_generator();
@@ -41,13 +41,13 @@ if (isset($_GET['action'])){
                 $action->action_layout();
             }
         }
-        
-        // index.php?action=generator&upload=true
         break;
         case 'download':
         require_once('controllers/Controller_download.php');
+                // bidouille pour fix quand taille non selectionnée
         if(!isset($_POST['textSize1'])) { $_POST['textSize1'] = 'medium';}
         if(!isset($_POST['textSize2'])) { $_POST['textSize2'] = 'medium';}
+                // route vers Generation Meme + download
         if ((isset($_POST['topText'])) 
             && (isset($_POST['bottomText'])) 
                 && (isset($_POST['textColor1'])) 
@@ -69,7 +69,7 @@ if (isset($_GET['action'])){
                             
             $action = new Controller_download($memeData);
             $action->genereAndDownload();
-
+                // route vers download d'un meme
         } elseif ((isset($_GET['memeId'])) && (isset($_GET['memeName']))){
 
             $action = new Controller_download();
@@ -81,7 +81,7 @@ if (isset($_GET['action'])){
         require_once('error.html');
     
     }
-} else {
+} else {        // page d'accueil par defaut
     require_once('controllers/Controller_home.php');
     $action = new Controller_home();
     $action->action_layout();
