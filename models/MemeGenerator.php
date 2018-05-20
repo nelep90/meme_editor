fd8eeda4da805b5e442f0556e314577d19ed5838
yannickACS <yannick.k@codeur.online>
2018-05-20 12:29:25 +0200

    Normalisation des marges, Ajout commentaires, Possibilité de traiter différents type de fonts avec l'ajout des propriétés fontTypes(tableau des fonts répertoriées) et fontType(string retour valeur utilisateur)

--- models/MemeGenerator.php
+++ models/MemeGenerator.php
@@ -10,9 +10,15 @@ class MemeGenerator{
     protected $textColor1;
     protected $textColor2;
     protected $id;
+    protected $fontTypes;// tableau des fonts disponibles
+    protected $fontType;// font sélectionnée
     const SMALL_FONT = 10;
-    const MEDIUM_FONT = 30;
+    const MEDIUM_FONT = 30;// taille police en pt
     const LARGE_FONT = 50;
+    const TEXT_MARGIN_TOP = 20;
+    const TEXT_MARGIN_RIGHT = 20;// Standardisation des marges
+    const TEXT_MARGIN_BOTTOM = 20;
+    const TEXT_MARGIN_LEFT = 20;
 
     public function __construct($id, $url, $topText, $bottomText, $fontSize, $fontSize2, $textColor1, $textColor2){
         $this->id = $id;
@@ -24,35 +30,45 @@ class MemeGenerator{
         $this->imageName = $url;
         $this->textColor1 = $this->toRGB($textColor1);
         $this->textColor2 = $this->toRGB($textColor2);
-        
+        $this->fontTypes = array('font1' => 'Lato-Bold');
+        $this->fontType = 'font1';
     }
     public function generateMemeFromJPG()
     { 
         $img=imagecreatefromjpeg($this->image);
         $img=$this->resize($img);
         putenv('GDFONTPATH=' . realpath('.' . '/assets/font/'));
-        $textcolor = imagecolorallocate($img, $this->textColor1[0], $this->textColor1[1], $this->textColor1[2]);
-
+        
+        // traitement topText
         if (strlen($this->topText) > 0){
-            $bbox = imagettfbbox($this->fontSize, 0, 'Lato-Bold', $this->topText);
+            // couleur texte top
+            $textcolor = imagecolorallocate($img, $this->textColor1[0], $this->textColor1[1], $this->textColor1[2]);
+            // on met le texte dans un rectangle virtuel pour prendre des mesures
+            $bbox = imagettfbbox($this->fontSize, 0, $this->fontTypes[$this->fontType], $this->topText);
+            // largeur texte
             $topTextSize = $bbox[2] - $bbox[0];
+            // hauteur texte
             $topTextHeight = $bbox[1] - $bbox[7];
+            // largeur image
             $imageWidth = imagesx($img);
-    
+            // calcul position x pour texte centré
             $xTopText = $imageWidth / 2 - $topTextSize / 2;
-            $yTopText = $topTextHeight + 20;
-            imagettftext($img, $this->fontSize, 0, $xTopText, $yTopText, $textcolor, 'Lato-Bold', $this->topText);
+            // calcul position y pour point en bas à gauche du texte + marge Top
+            $yTopText = $topTextHeight + self::TEXT_MARGIN_TOP;
+            // insertion du text dans l'image
+            imagettftext($img, $this->fontSize, 0, $xTopText, $yTopText, $textcolor, $this->fontTypes[$this->fontType], $this->topText);
         }
+        // traitement bottomText
         if (strlen($this->bottomText) > 0){
-            $bbox = imagettfbbox($this->fontSize2, 0, 'Lato-Bold', $this->bottomText);
+            $bbox = imagettfbbox($this->fontSize2, 0, $this->fontTypes[$this->fontType], $this->bottomText);
             $bottomTextcolor = imagecolorallocate($img, $this->textColor2[0], $this->textColor2[1], $this->textColor2[2]);
             $bottomTextSize = $bbox[2] - $bbox[0];
             $bottomTextHeight = $bbox[1] - $bbox[7];
             $imageWidth = imagesx($img);
             $imageHeight = imagesy($img);
             $xBottomText = $imageWidth / 2 - $bottomTextSize / 2;
-            $yBottomText = $imageHeight - 20;
-            imagettftext($img, $this->fontSize2, 0, $xBottomText, $yBottomText, $bottomTextcolor, 'Lato-Bold', $this->bottomText);
+            $yBottomText = $imageHeight - self::TEXT_MARGIN_BOTTOM;
+            imagettftext($img, $this->fontSize2, 0, $xBottomText, $yBottomText, $bottomTextcolor, $this->fontTypes[$this->fontType], $this->bottomText);
         }
         if ((strlen($this->topText) > 0) || (strlen($this->bottomText) > 0)){
             if (!file_exists($_SERVER["DOCUMENT_ROOT"] . "/meme_editor/assets/img/meme/meme_" . $this->imageName)){
