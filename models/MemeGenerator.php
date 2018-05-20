948ef8b227a8540f98a2719541ae387b83ecd1f5
yannickACS <yannick.k@codeur.online>
2018-05-20 20:45:37 +0200

    debut factorisation MemeGenerator

--- models/MemeGenerator.php
+++ models/MemeGenerator.php
@@ -4,14 +4,15 @@ class MemeGenerator{
     protected $image;
     protected $topText;
     protected $bottomText;
-    protected $fontSize;
-    protected $fontSize2;
+    protected $fontSize;// taille police texte top
+    protected $fontSize2;// taille police texte bottom
     protected $imageName;
-    protected $textColor1;
-    protected $textColor2;
+    protected $textColor1;// couleur texte top
+    protected $textColor2;// couleur text bot
     protected $id;
     protected $fontTypes;// tableau des fonts disponibles
-    protected $fontType;// font sélectionnée
+    protected $fontType1;// font sélectionnée top
+    protected $fontType2;// font sélectionnée bot
     const SMALL_FONT = 10;
     const MEDIUM_FONT = 30;// taille police en pt
     const LARGE_FONT = 50;
@@ -31,7 +32,8 @@ class MemeGenerator{
         $this->textColor1 = $this->toRGB($textColor1);
         $this->textColor2 = $this->toRGB($textColor2);
         $this->fontTypes = array('font1' => 'Lato-Bold');
-        $this->fontType = 'font1';
+        $this->fontType1 = 'font1';
+        $this->fontType2 = 'font1';
     }
     public function generateMemeFromJPG()
     { 
@@ -42,33 +44,33 @@ class MemeGenerator{
         // traitement topText
         if (strlen($this->topText) > 0){
             // couleur texte top
-            $textcolor = imagecolorallocate($img, $this->textColor1[0], $this->textColor1[1], $this->textColor1[2]);
+            $topTextColor = $this->set_textColor($img, $this->textColor1);
             // on met le texte dans un rectangle virtuel pour prendre des mesures
-            $bbox = imagettfbbox($this->fontSize, 0, $this->fontTypes[$this->fontType], $this->topText);
-            // largeur texte
-            $topTextSize = $bbox[2] - $bbox[0];
-            // hauteur texte
-            $topTextHeight = $bbox[1] - $bbox[7];
-            // largeur image
-            $imageWidth = imagesx($img);
-            // calcul position x pour texte centré
-            $xTopText = $imageWidth / 2 - $topTextSize / 2;
-            // calcul position y pour point en bas à gauche du texte + marge Top
-            $yTopText = $topTextHeight + self::TEXT_MARGIN_TOP;
-            // insertion du text dans l'image
-            imagettftext($img, $this->fontSize, 0, $xTopText, $yTopText, $textcolor, $this->fontTypes[$this->fontType], $this->topText);
+            // $bbox = imagettfbbox($this->fontSize, 0, $this->fontTypes[$this->fontType1], $this->topText);
+            // // largeur texte
+            // $topTextSize = $bbox[2] - $bbox[0];
+            // // hauteur texte
+            // $topTextHeight = $bbox[1] - $bbox[7];
+            // // largeur image
+            // $imageWidth = imagesx($img);
+            // // calcul position x pour texte centré
+            // $xTopText = $imageWidth / 2 - $topTextSize / 2;
+            // // calcul position y pour point en bas à gauche du texte + marge Top
+            // $yTopText = $topTextHeight + self::TEXT_MARGIN_TOP;
+            // // insertion du texte dans l'image
+            // imagettftext($img, $this->fontSize, 0, $xTopText, $yTopText, $textColor, $this->fontTypes[$this->fontType1], $this->topText);
+            $topTextSizes = $this->computeTextSize($img, $this->fontSize, $this->fontTypes[$this->fontType1], $this->topText);
+            $topTextInsert = $this->add_top_text($img, $topTextSizes, $topTextColor, $this->fontTypes[$this->fontType1], $this->fontSize, $this->topText);
         }
+        
         // traitement bottomText
         if (strlen($this->bottomText) > 0){
-            $bbox = imagettfbbox($this->fontSize2, 0, $this->fontTypes[$this->fontType], $this->bottomText);
-            $bottomTextcolor = imagecolorallocate($img, $this->textColor2[0], $this->textColor2[1], $this->textColor2[2]);
-            $bottomTextSize = $bbox[2] - $bbox[0];
-            $bottomTextHeight = $bbox[1] - $bbox[7];
-            $imageWidth = imagesx($img);
-            $imageHeight = imagesy($img);
-            $xBottomText = $imageWidth / 2 - $bottomTextSize / 2;
-            $yBottomText = $imageHeight - self::TEXT_MARGIN_BOTTOM;
-            imagettftext($img, $this->fontSize2, 0, $xBottomText, $yBottomText, $bottomTextcolor, $this->fontTypes[$this->fontType], $this->bottomText);
+
+            $bottomTextColor = $this->set_textColor($img, $this->textColor2);
+            $bottomTextSizes = $this->computeTextSize($img, $this->fontSize2, $this->fontTypes[$this->fontType2], $this->bottomText);
+            $bottomTextInsert = $this->add_bottom_text($img, $bottomTextSizes, $bottomTextColor, $this->fontTypes[$this->fontType2], $this->fontSize2, $this->bottomText);
+            
+            // $bottomTextInsert = $this->add_text($img, $this->fontSize2, $this->fontTypes[$this->fontType2], $this->bottomText, $bottomTextColor);
         }
         if ((strlen($this->topText) > 0) || (strlen($this->bottomText) > 0)){
             if (!file_exists($_SERVER["DOCUMENT_ROOT"] . "/meme_editor/assets/img/meme/meme_" . $this->imageName)){
@@ -85,6 +87,40 @@ class MemeGenerator{
             }
              
         }              
+    }
+    private function set_textColor($img, $rgb){
+        $textColor = imagecolorallocate($img, $rgb[0], $rgb[1], $rgb[2]);
+        return $textColor;
+    }
+    private function computeTextSize($img, $fontSize, $fontType, $text){
+        
+        // on met le texte dans un rectangle virtuel pour prendre des mesures
+        $bbox = imagettfbbox($fontSize, 0, $fontType, $text);
+        // largeur texte
+        $textWidth = $bbox[2] - $bbox[0];
+        // hauteur texte
+        $textHeight = $bbox[1] - $bbox[7];
+        // largeur image
+        $imageWidth = imagesx($img);
+        // hauteur image
+        $imageHeight = imagesy($img);
+        return ['textWidth' => $textWidth, 'textHeight' => $textHeight, 'imageWidth' => $imageWidth, 'imageHeight' => $imageHeight];
+    }
+    private function add_top_text($img, $sizes, $color, $fontType, $fontSize, $text){
+        // calcul position x pour texte centré
+        $xText = $sizes['imageWidth'] / 2 - $sizes['textWidth'] / 2;
+        // calcul position y pour point en bas à gauche du texte + marge Top
+        $yText = $sizes['textHeight'] + self::TEXT_MARGIN_TOP;
+        // insertion du texte dans l'image
+        return imagettftext($img, $fontSize, 0, $xText, $yText, $color, $fontType, $text);
+    }
+    private function add_bottom_text($img, $sizes, $color, $fontType, $fontSize, $text){
+        // calcul position x pour texte centré
+        $xText = $sizes['imageWidth'] / 2 - $sizes['textWidth'] / 2;
+        // calcul position y (hauteur image - marge bottom)
+        $yText = $sizes['imageHeight'] - self::TEXT_MARGIN_BOTTOM;
+        // insertion du texte dans l'image
+        return imagettftext($img, $fontSize, 0, $xText, $yText, $color, $fontType, $text);
     }
     public function generateMemeFromPNG()
     { 
