58f5072aa5fa9930c05d49f889f35c9a39f5e296
yannickACS <yannick.k@codeur.online>
2018-05-21 12:23:07 +0200

    Césure du texte haut et bas-> ok coté serveur, une galère sans nom, mon cerveau a fondu sur cette routine!

--- models/MemeGenerator.php
+++ models/MemeGenerator.php
@@ -38,72 +38,97 @@ class MemeGenerator{
     public function generateMemeFromJPG()
     { 
         $img=imagecreatefromjpeg($this->image);
+        // image max width 600;
         $img=$this->resize($img);
-        putenv('GDFONTPATH=' . realpath('.' . '/assets/font/'));
-        
+        putenv('GDFONTPATH=' . realpath('.' . '/assets/font/'));        
         // traitement topText
         if (strlen($this->topText) > 0){
             // couleur texte top
             $topTextColor = $this->set_textColor($img, $this->textColor1);
-            // on met le texte dans un rectangle virtuel pour prendre des mesures
-            // $bbox = imagettfbbox($this->fontSize, 0, $this->fontTypes[$this->fontType1], $this->topText);
-            // // largeur texte
-            // $topTextSize = $bbox[2] - $bbox[0];
-            // // hauteur texte
-            // $topTextHeight = $bbox[1] - $bbox[7];
-            // // largeur image
-            // $imageWidth = imagesx($img);
-            // // calcul position x pour texte centré
-            // $xTopText = $imageWidth / 2 - $topTextSize / 2;
-            // // calcul position y pour point en bas à gauche du texte + marge Top
-            // $yTopText = $topTextHeight + self::TEXT_MARGIN_TOP;
-            // // insertion du texte dans l'image
-            // imagettftext($img, $this->fontSize, 0, $xTopText, $yTopText, $textColor, $this->fontTypes[$this->fontType1], $this->topText);
-            $topTextSizes = $this->computeTextSize($img, $this->fontSize, $this->fontTypes[$this->fontType1], $this->topText);
-            $topTextInsert = $this->add_top_text($img, $topTextSizes, $topTextColor, $this->fontTypes[$this->fontType1], $this->fontSize, $this->topText);
-        }
-        
+            $textWrap = $this->textWrap($img, $this->fontSize, $this->fontTypes[$this->fontType1], $this->topText);
+            for ($i = 1; $i <= count($textWrap); $i++) {
+                // calcule des tailles retourne tableau ['textWidth' => $textWidth, 'textHeight' => $textHeight, 'imageWidth' => $imageWidth, 'imageHeight' => $imageHeight]
+                $topTextSizes = $this->computeTextSize($img, $this->fontSize, $this->fontTypes[$this->fontType1], $textWrap[$i - 1]);
+                $topTextSizes['textHeight'] = ($topTextSizes['textHeight'] * $i) + (($i - 1) * self::TEXT_MARGIN_TOP);
+                // insertion du texte dans l'image
+                $topTextInsert = $this->add_top_text($img, $topTextSizes, $topTextColor, $this->fontTypes[$this->fontType1], $this->fontSize, $textWrap[$i - 1]);
+            }
+        }        
         // traitement bottomText
         if (strlen($this->bottomText) > 0){
-
+            // couleur bottom texte
             $bottomTextColor = $this->set_textColor($img, $this->textColor2);
-            $bottomTextSizes = $this->computeTextSize($img, $this->fontSize2, $this->fontTypes[$this->fontType2], $this->bottomText);
-            $bottomTextInsert = $this->add_bottom_text($img, $bottomTextSizes, $bottomTextColor, $this->fontTypes[$this->fontType2], $this->fontSize2, $this->bottomText);
-            
-            // $bottomTextInsert = $this->add_text($img, $this->fontSize2, $this->fontTypes[$this->fontType2], $this->bottomText, $bottomTextColor);
+            $bottomTextWrap = $this->textWrap($img, $this->fontSize2, $this->fontTypes[$this->fontType2], $this->bottomText);
+            for ($i = 1; $i <= count($bottomTextWrap); $i++) {
+                // calcule des tailles retourne tableau ['textWidth' => $textWidth, 'textHeight' => $textHeight, 'imageWidth' => $imageWidth, 'imageHeight' => $imageHeight]
+                $bottomTextSizes = $this->computeTextSize($img, $this->fontSize2, $this->fontTypes[$this->fontType2], $bottomTextWrap[$i - 1]);
+                $bottomTextSizes['textHeight'] = (count($bottomTextWrap) - $i) * ($bottomTextSizes['textHeight'] + self::TEXT_MARGIN_BOTTOM);
+                // insertion du texte dans l'image
+                $topTextInsert = $this->add_bottom_text($img, $bottomTextSizes, $bottomTextColor, $this->fontTypes[$this->fontType2], $this->fontSize2, $bottomTextWrap[$i - 1]);
+            }
         }
+        // si il y a du texte en haut ou en bas
         if ((strlen($this->topText) > 0) || (strlen($this->bottomText) > 0)){
+            // si le nom du meme n'existe pas
             if (!file_exists($_SERVER["DOCUMENT_ROOT"] . "/meme_editor/assets/img/meme/meme_" . $this->imageName)){
                 imagejpeg($img, $_SERVER["DOCUMENT_ROOT"] . "/meme_editor/assets/img/meme/meme_" . $this->imageName);
         
                 imagedestroy($img);
                 return 'meme_' . $this->imageName;
+            // sinon trouver un autre nom
             } else {
                 $memeName = $this->findValidName(1);
                 imagejpeg($img, $_SERVER["DOCUMENT_ROOT"] . "/meme_editor/assets/img/meme/meme_" . $memeName);
         
                 imagedestroy($img);
                 return 'meme_' . $memeName;
-            }
-             
+            }            
         }              
     }
     private function set_textColor($img, $rgb){
         $textColor = imagecolorallocate($img, $rgb[0], $rgb[1], $rgb[2]);
         return $textColor;
     }
+    // Césure du texte, quelle misère!!!!!
+    private function textWrap($img, $fontSize, $fontType, $text){
+        $max_width = imagesx($img) - (self::TEXT_MARGIN_LEFT + self::TEXT_MARGIN_RIGHT);
+        // explose la string en tableau de chaque mots
+        $split = explode(" ", $text);
+
+        $string = "";
+        $new_string = "";
+        $textWrap = array();
+        for ($i = 0; $i < count($split); $i++){
+            $new_string .= $split[$i] . " ";
+            // vérif taille string
+            $tbbox = imagettfbbox($fontSize, 0, $fontType, $new_string);
+            if ($tbbox[4] < $max_width)
+            {
+                $string = $new_string;
+            } else {
+                $i --;
+                $textWrap[] = rtrim($string);
+                $string = '';
+                $new_string = '';
+            }
+            if (($i === (count($split) - 1)) && (strlen($string) > 0)){
+                $textWrap[] = rtrim($string);
+            }
+        }
+        return $textWrap;
+    }
     private function computeTextSize($img, $fontSize, $fontType, $text){
-        
+        // largeur image
+        $imageWidth = imagesx($img);
+        // hauteur image
+        $imageHeight = imagesy($img);
         // on met le texte dans un rectangle virtuel pour prendre des mesures
         $bbox = imagettfbbox($fontSize, 0, $fontType, $text);
         // largeur texte
         $textWidth = $bbox[2] - $bbox[0];
         // hauteur texte
         $textHeight = $bbox[1] - $bbox[7];
-        // largeur image
-        $imageWidth = imagesx($img);
-        // hauteur image
-        $imageHeight = imagesy($img);
+        
         return ['textWidth' => $textWidth, 'textHeight' => $textHeight, 'imageWidth' => $imageWidth, 'imageHeight' => $imageHeight];
     }
     private function add_top_text($img, $sizes, $color, $fontType, $fontSize, $text){
@@ -118,7 +143,7 @@ class MemeGenerator{
         // calcul position x pour texte centré
         $xText = $sizes['imageWidth'] / 2 - $sizes['textWidth'] / 2;
         // calcul position y (hauteur image - marge bottom)
-        $yText = $sizes['imageHeight'] - self::TEXT_MARGIN_BOTTOM;
+        $yText = ($sizes['imageHeight'] - self::TEXT_MARGIN_BOTTOM) - $sizes['textHeight'];
         // insertion du texte dans l'image
         return imagettftext($img, $fontSize, 0, $xText, $yText, $color, $fontType, $text);
     }
